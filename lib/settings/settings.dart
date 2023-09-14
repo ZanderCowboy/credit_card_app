a7eb24e4cc8bdd426d0d045c15d03697fac0ac9e
ZanderCowboy <zanderkotze99@gmail.com>
2023-09-14 16:01:46 +0200

    Refactored code and created bloc for Settings.

---
+++ lib/settings/settings.dart
@@ -0,0 +1,2 @@
+import 'bloc/settings_bloc.dart';
+import 'view/settings_page.dart';
---
+++ lib/settings/view/settings_page.dart
@@ -0,0 +1,13 @@
+// This page will contain an entry 'Banned Countries', where the admin can select any countries that are banned. 
+// Default will be no countries.
+
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
