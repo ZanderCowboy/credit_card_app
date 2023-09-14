897b96846e40c59ae9a121105adeea9787a107ce
ZanderCowboy <zanderkotze99@gmail.com>
2023-09-14 16:02:59 +0200

    Created a constants file.

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -1,3 +1,32 @@
 // This should contain a list of banned countries. 
 
 // This should contain an enum with the Card Types.
+
+// routes
+const String initialRoute = '/';
+const String homeRoute = '/home';
+const String enterRoute = '/enter';
+const String scanRoute = '/scan';
+const String historyRoute = '/history';
+const String settingsRoute = '/settings';
+
+// AppBar Titles
+const String startAppBarTitle = 'Start App Bar';
+const String homeAppBarTitle = 'Home App Bar';
+const String enterAppBarTitle = 'Enter App Bar';
+const String scanAppBarTitle = 'Scan App Bar';
+const String historyAppBarTitle = 'History App Bar';
+const String resultAppBarTitle = 'Result App Bar';
+
+// Button Titles
+const String startButtonTitle = 'Start';
+const String homeButtonTitle = 'Home';
+const String enterButtonTitle = 'Enter';
+const String scanButtonTitle = 'Scan';
+const String historyButtonTitle = 'History';
+const String resultButtonTitle = 'Result';
+
+// Drawer Tile Titles
+const String settingsTitle = 'Settings';
+const String bannedCountriesTitle = 'Banned Countries';
+const String homeLogout = 'Logout';
