053cbbfcdbe8a975de050d958bca21656dada39d
ZanderCowboy <zanderkotze99@gmail.com>
2023-09-19 19:55:25 +0200

    Renamed and removed files. Uncommented unnecessary code.

--- lib/components/capture.dart
+++ lib/components/capture.dart
@@ -1,47 +1,47 @@
 // This file will be used to capture and hold the details temporarily before being saved.
 
 
-import 'package:flutter/material.dart';
-import 'package:credit_card_app/credit_card/credit_card_details.dart';
+// import 'package:flutter/material.dart';
+// import 'package:credit_card_app/credit_card/credit_card_details.dart';
 
-CreditCard creditCard = CreditCard(
-  cardNumber: 0,
-  cardType: '',
-  cvvNumber: 0,
-  issuingCountry: 'issuingCountry',
-);
+// CreditCard creditCard = CreditCard(
+//   cardNumber: 0,
+//   cardType: '',
+//   cvvNumber: 0,
+//   issuingCountry: 'issuingCountry',
+// );
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Material App',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Material App Bar'),
+//         ),
+//         body: const Center(
+//           child: Text('Hello World'),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class Capture extends StatefulWidget {
-  const Capture({super.key});
+// class Capture extends StatefulWidget {
+//   const Capture({super.key});
 
-  @override
-  State<Capture> createState() => _CaptureState();
-}
+//   @override
+//   State<Capture> createState() => _CaptureState();
+// }
 
-class _CaptureState extends State<Capture> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+// class _CaptureState extends State<Capture> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
