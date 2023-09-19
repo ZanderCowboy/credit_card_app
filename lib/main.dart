61e02a2c4e278d6eb2fa3f50dfaedcde437d99be
ZanderCowboy <zanderkotze99@gmail.com>
2023-09-19 19:54:36 +0200

    Implemented a simplified observer.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 
+import 'package:credit_card_app/domain/credit_card/credit_card_repository.dart';
+import 'package:credit_card_app/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app/view/app.dart';
 
 void main() {
-  runApp(const App());
+  Bloc.observer = const SimpleBlocObserver();
+  runApp(App(creditCardRepository: CreditCardRepository(),));
 }
