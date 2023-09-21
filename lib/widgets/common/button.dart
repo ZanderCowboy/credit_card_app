9ef5888158fb4bfaf842f01b8216da42c5e7c7fb
ZanderCowboy <zanderkotze99@gmail.com>
2023-09-21 19:18:09 +0200

    Renamed enter_page copies; Added new button; Added assets;

--- lib/widgets/common/button.dart
+++ lib/widgets/common/button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Button extends StatelessWidget {
-  const Button({
+class ButtonLarge extends StatelessWidget {
+  const ButtonLarge({
     super.key,
     required this.routeName,
     required this.text,
@@ -18,10 +18,45 @@ class Button extends StatelessWidget {
           fontSize: 22.0,
           fontWeight: FontWeight.bold,
         ),
-        minimumSize: const Size(200, 80),
+        minimumSize: const Size(200, 65),
       ),
       onPressed: () => Navigator.of(context).pushNamed(routeName),
       child: Text(text),
     );
   }
+}
+
+class ButtonSmall extends StatelessWidget {
+  const ButtonSmall({
+    super.key,
+    required this.routeName,
+    required this.text,
+    // this.function,
+  });
+
+  final String routeName;
+  final String text;
+  // void function;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: buttonSmallStyle,
+      onPressed: () => Navigator.of(context).pushNamed(routeName),
+      // onPressed: () {
+
+      // },
+      child: Text(text),
+    );
+  }
+}
+
+ButtonStyle get buttonSmallStyle {
+  return ElevatedButton.styleFrom(
+    textStyle: const TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.bold,
+    ),
+    minimumSize: const Size(160, 40),
+  );
 }
