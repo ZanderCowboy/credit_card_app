12999c4f213b3e1a97f0a53a59bc1f2264ecf642
ZanderCowboy <zanderkotze99@gmail.com>
2023-09-26 01:18:29 +0200

    Added db_driver and Hive box implementations; Implemented changes for Hive DB into repositories; Added auto-generated files.

--- lib/domain/banned_countries/models/banned_countries.dart
+++ lib/domain/banned_countries/models/banned_countries.dart
@@ -1,7 +1,15 @@
 // This file will contain the data layer for the banned countries.
 
-// class Country {
+import 'package:hive/hive.dart';
 
-// }
+part 'banned_countries.g.dart';
 
-String country = '';
+@HiveType(typeId: 1)
+class BannedCountries {
+  BannedCountries({required this.bannedCountry, required this.isBanned});
+
+  @HiveField(0)
+  final String bannedCountry;
+  @HiveField(1)
+  final bool isBanned;
+}
--- lib/domain/credit_card/models/credit_card.dart
+++ lib/domain/credit_card/models/credit_card.dart
@@ -1,5 +1,10 @@
 // This file should contain all the details that are gathered from either capturing or scanning a credit card.
 
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'credit_card.g.dart';
+
+@HiveType(typeId: 0)
 class CreditCard {
   const CreditCard({
     required this.cardNumber,
@@ -8,8 +13,12 @@ class CreditCard {
     required this.issuingCountry,
   });
 
+  @HiveField(0)
   final String cardNumber;
+  @HiveField(1)
   final String cardType;
+  @HiveField(2)
   final int cvvNumber;
+  @HiveField(3)
   final String issuingCountry;
 }
