5d1b9db5a6cf9216b6426b6dcd691e8f5f6e37f7
ZanderCowboy <zanderkotze99@gmail.com>
2023-09-26 17:46:23 +0200

    Added .yml actions for GitHub Actions; Fixed test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:credit_card_app/domain/banned_countries/banned_countries_repository.dart';
+import 'package:credit_card_app/domain/credit_card/credit_card_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,8 +14,15 @@ import 'package:credit_card_app/app/view/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    CreditCardRepository cardRepository = CreditCardRepository();
+    BannedCountriesRepository bannedCountriesRepository =
+        BannedCountriesRepository();
+
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(App(
+      creditCardRepository: cardRepository,
+      bannedCountriesRepository: bannedCountriesRepository,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
