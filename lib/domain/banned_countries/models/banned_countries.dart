667feb3810f92e29e6ca72b541058cdf3e6da10a
ZanderCowboy <zanderkotze99@gmail.com>
2023-10-05 22:06:08 +0200

    Changed model to use freezed.

--- lib/domain/banned_countries/models/banned_countries.dart
+++ lib/domain/banned_countries/models/banned_countries.dart
@@ -1,15 +1,22 @@
 // This file will contain the data layer for the banned countries.
 
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hive/hive.dart';
+
+part 'banned_countries.freezed.dart';
 
 part 'banned_countries.g.dart';
 
 @HiveType(typeId: 1)
-class BannedCountries {
-  BannedCountries({required this.bannedCountry, required this.isBanned});
+@freezed
+class BannedCountries with _$BannedCountries {
+  const factory BannedCountries({
+    @HiveField(0) required String bannedCountry,
+    @HiveField(1) required bool isBanned,
+  }) = _BannedCountries;
 
-  @HiveField(0)
-  final String bannedCountry;
-  @HiveField(1)
-  final bool isBanned;
+  factory BannedCountries.empty() => const BannedCountries(
+        bannedCountry: '',
+        isBanned: false,
+      );
 }
---
+++ lib/domain/banned_countries/models/banned_countries.freezed.dart
@@ -0,0 +1,161 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'banned_countries.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$BannedCountries {
+  @HiveField(0)
+  String get bannedCountry => throw _privateConstructorUsedError;
+  @HiveField(1)
+  bool get isBanned => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $BannedCountriesCopyWith<BannedCountries> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BannedCountriesCopyWith<$Res> {
+  factory $BannedCountriesCopyWith(
+          BannedCountries value, $Res Function(BannedCountries) then) =
+      _$BannedCountriesCopyWithImpl<$Res, BannedCountries>;
+  @useResult
+  $Res call({@HiveField(0) String bannedCountry, @HiveField(1) bool isBanned});
+}
+
+/// @nodoc
+class _$BannedCountriesCopyWithImpl<$Res, $Val extends BannedCountries>
+    implements $BannedCountriesCopyWith<$Res> {
+  _$BannedCountriesCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? bannedCountry = null,
+    Object? isBanned = null,
+  }) {
+    return _then(_value.copyWith(
+      bannedCountry: null == bannedCountry
+          ? _value.bannedCountry
+          : bannedCountry // ignore: cast_nullable_to_non_nullable
+              as String,
+      isBanned: null == isBanned
+          ? _value.isBanned
+          : isBanned // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$BannedCountriesImplCopyWith<$Res>
+    implements $BannedCountriesCopyWith<$Res> {
+  factory _$$BannedCountriesImplCopyWith(_$BannedCountriesImpl value,
+          $Res Function(_$BannedCountriesImpl) then) =
+      __$$BannedCountriesImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({@HiveField(0) String bannedCountry, @HiveField(1) bool isBanned});
+}
+
+/// @nodoc
+class __$$BannedCountriesImplCopyWithImpl<$Res>
+    extends _$BannedCountriesCopyWithImpl<$Res, _$BannedCountriesImpl>
+    implements _$$BannedCountriesImplCopyWith<$Res> {
+  __$$BannedCountriesImplCopyWithImpl(
+      _$BannedCountriesImpl _value, $Res Function(_$BannedCountriesImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? bannedCountry = null,
+    Object? isBanned = null,
+  }) {
+    return _then(_$BannedCountriesImpl(
+      bannedCountry: null == bannedCountry
+          ? _value.bannedCountry
+          : bannedCountry // ignore: cast_nullable_to_non_nullable
+              as String,
+      isBanned: null == isBanned
+          ? _value.isBanned
+          : isBanned // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$BannedCountriesImpl implements _BannedCountries {
+  const _$BannedCountriesImpl(
+      {@HiveField(0) required this.bannedCountry,
+      @HiveField(1) required this.isBanned});
+
+  @override
+  @HiveField(0)
+  final String bannedCountry;
+  @override
+  @HiveField(1)
+  final bool isBanned;
+
+  @override
+  String toString() {
+    return 'BannedCountries(bannedCountry: $bannedCountry, isBanned: $isBanned)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$BannedCountriesImpl &&
+            (identical(other.bannedCountry, bannedCountry) ||
+                other.bannedCountry == bannedCountry) &&
+            (identical(other.isBanned, isBanned) ||
+                other.isBanned == isBanned));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, bannedCountry, isBanned);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$BannedCountriesImplCopyWith<_$BannedCountriesImpl> get copyWith =>
+      __$$BannedCountriesImplCopyWithImpl<_$BannedCountriesImpl>(
+          this, _$identity);
+}
+
+abstract class _BannedCountries implements BannedCountries {
+  const factory _BannedCountries(
+      {@HiveField(0) required final String bannedCountry,
+      @HiveField(1) required final bool isBanned}) = _$BannedCountriesImpl;
+
+  @override
+  @HiveField(0)
+  String get bannedCountry;
+  @override
+  @HiveField(1)
+  bool get isBanned;
+  @override
+  @JsonKey(ignore: true)
+  _$$BannedCountriesImplCopyWith<_$BannedCountriesImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
