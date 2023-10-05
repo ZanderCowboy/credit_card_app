557bdfc0c8d24b7ecf9ea6a2e4f08bf2e2d268a4
ZanderCowboy <zanderkotze99@gmail.com>
2023-10-05 22:35:39 +0200

    Added very_good_analysis to code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:credit_card_app/app/view/app.dart';
 import 'package:credit_card_app/data/persistance/db_driver.dart';
 import 'package:credit_card_app/get_it_injection.dart';
 import 'package:credit_card_app/infrastructure/banned_countries/banned_countries_repository.dart';
@@ -5,7 +6,7 @@ import 'package:credit_card_app/infrastructure/credit_card/credit_card_repositor
 import 'package:credit_card_app/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'app/view/app.dart';
+import 'package:very_good_analysis/very_good_analysis.dart';
 
 const String creditCardBoxName = "credit_cards";
 const String bannedCountriesBoxName = "banned_countries";
