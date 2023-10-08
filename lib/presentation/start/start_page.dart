6e392420f893e4b0d5fcac6a5aa0e444a7894f2d
ZanderCowboy <zanderkotze99@gmail.com>
2023-10-08 12:08:32 +0200

    Added a background LogoWidget to StartPage.

--- lib/presentation/start/start_page.dart
+++ lib/presentation/start/start_page.dart
@@ -1,21 +1,91 @@
+import 'dart:ui';
+
 import 'package:credit_card_app/constants/constants.dart';
 import 'package:credit_card_app/widgets/common/button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ButtonLarge(routeName: homeRoute, text: startButtonTitle),
-          ],
-        ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          _LogoWidget(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ButtonLarge(routeName: homeRoute, text: startButtonTitle),
+              ],
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _LogoWidget extends StatelessWidget {
+  const _LogoWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          color: Color(0xFFAF8D45),
+          width: double.infinity,
+          height: double.infinity,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // SizedBox(
+              //   height: 20,
+              // ),
+              // Image.asset(
+              //   'assets/logo.png',
+              //   fit: BoxFit.cover,
+              //   // fit: BoxFit.scaleDown,
+              // ),
+              // BackdropFilter(
+              //   filter: ImageFilter.blur(
+              //     sigmaX: 0.5,
+              //     sigmaY: 0.5,
+              //   ),
+              //   child: Container(
+              //     color: Colors.white.withOpacity(0.05),
+              //   ),
+              // ),
+              // SizedBox(
+              //   height: 90,
+              // ),
+              ImageFiltered(
+                imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Center(
+                  child: Text(
+                    'RANK GROUP',
+                    style: GoogleFonts.ubuntu(
+                      textStyle: TextStyle(
+                        fontSize: 90,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                        color: Colors.white,
+                      ),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
