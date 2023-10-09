ab98d72d1556876cbe3a3aad516f084017421f36
ZanderCowboy <zanderkotze99@gmail.com>
2023-10-10 00:03:51 +0200

    Added detectCCType method and code for onTap events.

--- lib/application/enter/bloc/enter_bloc.dart
+++ lib/application/enter/bloc/enter_bloc.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:credit_card_app/constants/credit_card_number_patterns.dart';
 import 'package:credit_card_app/domain/credit_card/i_credit_card_repository.dart';
 import 'package:credit_card_app/domain/credit_card/models/credit_card.dart';
 import 'package:flutter_credit_card/extension.dart';
@@ -99,7 +102,11 @@ class EnterBloc extends Bloc<EnterEvent, EnterState> {
             emit(EnterState.initial());
           },
           onChangedCardNumber: (value) {
-            emit(state.copyWith(isLoading: true, errorMessage: null));
+            emit(state.copyWith(
+              isLoading: true,
+              isCvvFocused: false,
+              errorMessage: null,
+            ));
             emit(
               state.copyWith(
                 creditCard: state.creditCard.copyWith(
@@ -107,10 +114,15 @@ class EnterBloc extends Bloc<EnterEvent, EnterState> {
                 ),
               ),
             );
+
+            CardTypes cardTypes = detectCCType(value.text);
+            log('cardTypes=${cardTypes.name}');
+
             final isValidTextValue = _isValid(state.creditCard);
             emit(
               state.copyWith(
                 creditCard: state.creditCard.copyWith(
+                  cardType: cardTypes.name,
                   isValid: isValidTextValue,
                 ),
               ),
@@ -137,11 +149,15 @@ class EnterBloc extends Bloc<EnterEvent, EnterState> {
             emit(state.copyWith(isLoading: false));
           },
           onChangedCvvNumber: (value) {
-            emit(state.copyWith(isLoading: true, errorMessage: null));
+            emit(state.copyWith(
+              isLoading: true,
+              errorMessage: null,
+            ));
             if (value.text.isNotEmpty) {
               emit(
                 state.copyWith(
                   creditCard: state.creditCard.copyWith(cvvNumber: value.text),
+                  isCvvFocused: true,
                 ),
               );
             }
@@ -174,6 +190,18 @@ class EnterBloc extends Bloc<EnterEvent, EnterState> {
               ),
             );
             emit(state.copyWith(isLoading: false));
+          },
+          onTapCardNumber: (value) {
+            emit(state.copyWith(isCvvFocused: false));
+          },
+          onTapCardType: (value) {
+            emit(state.copyWith(isCvvFocused: false));
+          },
+          onTapCvvNumber: (_) {
+            emit(state.copyWith(isCvvFocused: true));
+          },
+          onTapIssuingCountry: (value) {
+            emit(state.copyWith(isCvvFocused: false));
           },
         );
       },
@@ -208,4 +236,51 @@ class EnterBloc extends Bloc<EnterEvent, EnterState> {
 
     return [true, ''];
   }
+}
+
+CardTypes detectCCType(String cardNumber) {
+  //Default card type is other
+  CardTypes cardType = CardTypes.otherBrand;
+
+  if (cardNumber.isEmpty) {
+    return cardType;
+  }
+
+  cardNumPatterns.forEach(
+    (CardTypes type, Set<List<String>> patterns) {
+      for (List<String> patternRange in patterns) {
+        // Remove any spaces
+        String ccPatternStr = cardNumber.replaceAll(RegExp(r'\s+\b|\b\s'), '');
+        final int rangeLen = patternRange[0].length;
+        // Trim the Credit Card number string to match the pattern prefix length
+        if (rangeLen < cardNumber.length) {
+          ccPatternStr = ccPatternStr.substring(0, rangeLen);
+        }
+
+        if (patternRange.length > 1) {
+          // Convert the prefix range into numbers then make sure the
+          // Credit Card num is in the pattern range.
+          // Because Strings don't have '>=' type operators
+          final int ccPrefixAsInt = int.parse(ccPatternStr);
+          final int startPatternPrefixAsInt = int.parse(patternRange[0]);
+          final int endPatternPrefixAsInt = int.parse(patternRange[1]);
+          if (ccPrefixAsInt >= startPatternPrefixAsInt &&
+              ccPrefixAsInt <= endPatternPrefixAsInt) {
+            // Found a match
+            cardType = type;
+            break;
+          }
+        } else {
+          // Just compare the single pattern prefix with the Credit Card prefix
+          if (ccPatternStr == patternRange[0]) {
+            // Found a match
+            cardType = type;
+            break;
+          }
+        }
+      }
+    },
+  );
+
+  return cardType;
 }
